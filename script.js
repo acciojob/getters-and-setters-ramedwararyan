d950699ab1cc57a71c6c0f55c6851e664a8adbb7
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-07-26 03:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+    this._name = name;  // use underscore to avoid conflicts with getter
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+	 study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+	teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
